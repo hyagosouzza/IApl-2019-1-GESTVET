78ac5d133d2f4b1f848cf3eb1deea870997f5e12
Hyago Souza <hyagosouzza@hotmail.com>
2019-04-29 09:24:04 -0300

    write.java

--- AS01/Sistemas/gestvet/src/main/java/File/Write.java
+++ AS01/Sistemas/gestvet/src/main/java/File/Write.java
@@ -12,8 +12,6 @@ import org.json.JSONObject;
 import java.io.BufferedWriter;
 import java.io.FileWriter;
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
 
 public class Write {
