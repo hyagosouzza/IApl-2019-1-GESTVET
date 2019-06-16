dccc7525a4c562a89ebb155aec4a2fb5052cd246
Pedro Henrique <pedro.hc.silva@outlook.com>
2019-06-16 13:39:45 -0300

    Internacionalização - avisos

--- AS04/gestvet-front/src/app/messages/messages.ts
+++ AS04/gestvet-front/src/app/messages/messages.ts
@@ -48,6 +48,20 @@ export class Messages {
                 user: 'Usuário',
                 password: 'Senha',
                 signIn: 'Logar'
+            },
+            notifications:{
+                createAnimalName: 'O campo nome precisa ser preenchido',
+                createAnimalSpecie: 'O campo tipo precisa ser preenchido',
+                createAnimalAge: 'O campo idade precisa ser preenchido',
+                createAnimalBreed: 'O campo raça precisa ser preenchido',
+                createDrugName: 'O campo nome precisa ser preenchido',
+                createDrugPrice: 'O campo preço precisa ser preenchido',
+                createDrugDosage: 'O campo dosagem precisa ser preenchido',
+                createDrugQuantity: 'O campo quantidade precisa ser preenchido',
+                loginSucess: 'Autenticação realizada',
+                logout: 'Seção encerrada',
+                loginName: 'Preencha o campo usuário',
+                loginPassword: 'Preencha o campo senha'
             }
         },
         en:{
@@ -98,6 +112,20 @@ export class Messages {
                 user: 'User',
                 password: 'Password',
                 signIn: 'Sign In'
+            },
+            notifications:{
+                createAnimalName: 'Name field must be filled in',
+                createAnimalSpecie: 'Type field must be filled in',
+                createAnimalAge: 'Age field needs to be filled',
+                createAnimalBreed: 'Breed field needs to be filled',
+                createDrugName: 'Name field must be filled in',
+                createDrugPrice: 'Price field needs to be filled in',
+                createDrugDosage: 'Dosage field needs to be filled in',
+                createDrugQuantity: 'Quantity field needs to be filled in',
+                loginSucess: 'Authentication performed',
+                logout: 'Section closed',
+                loginName: 'Fill in the user field',
+                loginPassword: 'Fill in the password field'
             }
         },
         es:{
@@ -148,6 +176,20 @@ export class Messages {
                 user: 'Usuario',
                 password: 'Contraseña',
                 signIn: 'Sign In'
+            },
+            notifications:{
+                createAnimalName: 'El campo de nombre se debe rellenar',
+                createAnimalSpecie: 'El campo tipo debe llenarse',
+                createAnimalAge: 'El campo edad debe llenarse',
+                createAnimalBreed: 'El campo raza debe llenarse',
+                createDrugName: 'El campo de nombre se debe rellenar',
+                createDrugPrice: 'El campo precio debe llenarse',
+                createDrugDosage: 'El campo dosificación debe llenarse',
+                createDrugQuantity: 'El campo cantidad debe llenarse',
+                loginSucess: 'Autenticación realizada',
+                logout: 'Sección cerrada',
+                loginName: 'Rellene el campo de usuario',
+                loginPassword: 'Rellene el campo contraseña'
             }
         }
     }
